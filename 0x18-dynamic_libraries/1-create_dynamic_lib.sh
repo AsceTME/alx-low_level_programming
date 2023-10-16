238b64797bd2210082f8224309397eb8aadbcc79
AsceTME <veronsmall2002@gmail.com>
2023-10-16 12:16:14 +0200

    New Finding

--- 0x18-dynamic_libraries/1-create_dynamic_lib.sh
+++ 0x18-dynamic_libraries/1-create_dynamic_lib.sh
@@ -1,3 +1,10 @@
 #!/bin/bash
-gcc *.c -c -fpic
+
+# Compile all .c files into object files
+gcc -Wall -Werror -Wextra -pedantic -std=gnu89 -fPIC -c *.c
+
+# Create the dynamic library
 gcc -shared -o liball.so *.o
+
+# Clean up the object files
+rm *.o
